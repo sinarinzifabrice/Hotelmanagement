20f00e934562ee47122b4cb490e3c2aa311c86bd
fabrice sinarinzi <sinarinzifabrice@gmail.com>
2024-11-19 23:51:31 -0500

    Create backend and frotend projects

---
+++ HotelServer/src/test/java/com/codeWithProject/HotelServer/HotelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeWithProject.HotelServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
